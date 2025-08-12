2622fb70b9f3732e8026ccc4ee23e89f4af9ed40
Mattisback04 <mwl140@msstate.edu>
2025-08-12 10:54:31 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/MattTheMet.github.io/',
+  base: '/MattTheMet/',
   plugins: [react()],
 });
 
